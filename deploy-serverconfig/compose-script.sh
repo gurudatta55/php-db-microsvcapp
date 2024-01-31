06b9f5fa9fb96767d152613f704bf02891631f2f
gurudatta55 <gurudatta.aws@gmail.com>
2024-01-31 23:50:43 +0530

    index file moved

--- deploy-serverconfig/compose-script.sh
+++ deploy-serverconfig/compose-script.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo DOCKER_PHP_IMAGE=$1 docker-compose -f /home/ec2-user/deploy-serverconfig/docker-compose.yml up -d
+sudo DOCKER_PHP_IMAGE=$1 /usr/local/bin/docker-compose -f /home/ec2-user/deploy-serverconfig/docker-compose.yml up -d
